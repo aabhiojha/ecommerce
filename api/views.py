7a0ce618243147eaf6a6936eb37009daf24a67d7
Abhishek Ojha <aabhiojha08@outlook.com>
2025-08-10 00:41:17 -0700

    feat: Added order prefetching and silk middleware

--- api/views.py
+++ api/views.py
@@ -23,8 +23,13 @@ def product_detail(request, id):
 
 @api_view(["GET"])
 def order_list(request):
-    order = Order.objects.all()
-    serializer = OrderSerializer(order, many=True)
+
+    orders = Order.objects.prefetch_related(
+        "items",
+        "items__product",
+    )
+
+    serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
 
 
