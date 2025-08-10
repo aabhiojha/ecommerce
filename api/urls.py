ef514664667cf0ff11554bd5b052c34dc273d6e2
Abhishek Ojha <aabhiojha08@outlook.com>
2025-08-10 02:44:35 -0700

    feat(api): add user-order endpoint and authentication for orders

--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("product/info/", views.product_info),
     path("product_detail/<int:product_id>/", views.ProductDetailAPIView.as_view()),
     path("orders/", views.OrderListAPIView.as_view()),
+    path("user-orders/", views.UserOrderListAPIView.as_view(), name="user-orders"),
 ]
--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.db.models import Max
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from api.models import Order, OrderItem, Product
@@ -26,6 +27,16 @@ class OrderListAPIView(generics.ListAPIView):
         "items__product",
     )
     serializer_class = OrderSerializer
+
+
+class UserOrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related("items__product")
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(user=self.request.user)
 
 
 @api_view(["GET"])
